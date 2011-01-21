b6e321a97f629adb39e78fd26ff06b661b3921b9
devgru@nemetz <devgru@mail.ru>
2011-01-21 20:13:45 +0300

    + bufferization

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 var colors = require('./colors.js');
 
-exports.forContext = function (context, color) { return new Logger(context, color) };
-exports.setPadding = function (padding) { Logger.padding = padding; };
-
 var Logger = function (context, contextColor) {
-
     Logger.padding = Math.max(Logger.padding, context.length);
 
-    contextColor = contextColor ? colors.bold[contextColor] : colors.bold.white;
+    contextColorCode = contextColor ? colors.bold[contextColor] : colors.bold.white;
 
-    var log = function (text, textColor) {
-        if (typeof(text) == "object") text = JSON.stringify(text);
-        doPadding();
-        console.log('[' + colorize(context, contextColor) + '] ' + colorize(text, textColor));
-    };
-
+    var that = this;
     var doPadding = function () {
         while (context.length < Logger.padding) {
-            if (context.length % 2) context += ' ';
-            else context = ' ' + context;
+            context = context.length % 2 ? context + ' ' : ' ' + context;
         }
     };
-
+    var prepareText = function (text) {
+        doPadding();
+        return typeof(text) == "object" ? JSON.stringify(text) : text;
+    };
+    var log = function (text, textColor) {
+        that.rawPrint(colorize(prepareText(text), textColor));
+        return that;
+    };
     var colorize = function (text, color) { return color + text + colors.reset; };
 
-    this.info   = function (text) { log(text, colors.white); };
-    this.debug  = function (text) { if (this.showDebug) log(text, colors.cyan); };
-    this.error  = function (text) { log(text, colors.bold.red); };
-    this.warn   = function (text) { log(text, colors.yellow); };
-    this.call   = null;
+    this.rawPrint = function (text) {
+        console.log('[' + colorize(context, contextColorCode) + '] ' + text);
+    };
 
-    this.showDebug = true;
-}
+    this.info   = function (text) { return log(text, colors.white);};
+    this.debug  = function (text) { return this.showDebug ? log(text, colors.cyan) : this; };
+    this.error  = function (text) { return log(text, colors.bold.red); };
+    this.warn   = function (text) { return log(text, colors.yellow); };
 
-Logger.padding = 0; //tune it if you need
+    this.buffered = function () {
+        var chain = new Logger(context, contextColor);
+        chain.buffer = '';
+        chain.oldRawPrint = chain.rawPrint;
+        chain.rawPrint = function (text) { this.buffer += text + ' '; };
+        chain.flush = function() {
+            chain.oldRawPrint(this.buffer);
+            this.buffer = '';
+        };
+
+        return chain;
+    };
+
+    this.showDebug = Logger.defaultShowDebug;
+};
+
+Logger.padding = 0;
+Logger.defaultShowDebug = true;
+
+exports.forContext = function (context, color) { return new Logger(context, color); };
+exports.setPadding = function (padding) { Logger.padding = Math.max(padding, Logger.padding); };
+exports.setDefaultShowDebug = function (show) { Logger.defaultShowDebug = show; };
